6246abd5c8c4c70762ddcb1e28a177189245761a
ILIA <ikpolushin@yandex.ru>
2026-03-27 12:34:32 +0300

    test routes

---
+++ notes/tests/test_routes.py
@@ -0,0 +1,88 @@
+from http import HTTPStatus
+
+from django.test import TestCase, Client
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+
+User = get_user_model()
+
+
+class TestRoutes(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        cls.user = User.objects.create(username='some_user')
+        cls.auth_client = Client()
+        cls.login_url = reverse('users:login')
+
+    def test_authorised_user(self):
+
+        test_sets = (
+            {
+                'url_name': 'notes:home',
+                'exp_result': HTTPStatus.OK,
+            },
+            {
+                'url_name': 'notes:list',
+                'exp_result': HTTPStatus.OK,
+            },
+            {
+                'url_name': 'notes:add',
+                'exp_result': HTTPStatus.OK,
+            },
+        )
+
+        for test_set in test_sets:
+            url = test_set['url_name']
+            exp_result = test_set['exp_result']
+
+            with self.subTest(name=url):
+                self.auth_client.force_login(self.user)
+                url = reverse(url)
+                response = self.auth_client.get(url)
+                self.assertEqual(response.status_code, exp_result)
+
+    def test_unauthorized_user(self):
+        test_sets = (
+            {
+                'url_name': 'notes:home',
+                'exp_result': HTTPStatus.OK,
+            },
+            {
+                'url_name': 'notes:list',
+                'exp_result': HTTPStatus.FOUND,
+            },
+            {
+                'url_name': 'notes:add',
+                'exp_result': HTTPStatus.FOUND,
+            },
+        )
+
+        for test_set in test_sets:
+            url = test_set['url_name']
+            exp_result = test_set['exp_result']
+
+            with self.subTest(name=url):
+                url = reverse(url)
+                response = self.client.get(url)
+                self.assertEqual(response.status_code, exp_result)
+
+    def test_notes_page_unauthorized_user_redirect(self):
+
+        test_sets = (
+            {
+                'url_name': 'notes:list',
+            },
+            {
+                'url_name': 'notes:add',
+            },
+        )
+
+        for test_set in test_sets:
+            url = test_set['url_name']
+
+            with self.subTest(name=url):
+                url = reverse(url)
+                response = self.client.get(url)
+                redirect_url = f'{self.login_url}?next={url}'
+                self.assertRedirects(response, redirect_url)
