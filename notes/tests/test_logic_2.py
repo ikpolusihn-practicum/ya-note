87de1afb7957900e7f10313739009b8b80e594e9
ikpolushin <ikpolushin@yandex.ru>
2026-04-03 15:55:38 +0300

    test other_user_cant_delete_note

--- notes/tests/test_logic_2.py
+++ notes/tests/test_logic_2.py
@@ -1,8 +1,9 @@
 import pytils
+from http import HTTPStatus
 
 from django.contrib.auth import get_user_model
 from django.urls import reverse
-from django.test import TestCase
+from django.test import TestCase, Client
 from django.core.exceptions import ValidationError
 
 from notes.models import Note
@@ -18,10 +19,17 @@ class TestLogic(TestCase):
         cls.note_author = User.objects.create(username='note_author')
         cls.login_url = reverse('users:login')
 
-        cls.new_note_1_data = {
-            'title': 'new_note_title',
-            'text': 'new_note_text',
-            'slug': 'new_note_slug',
+        cls.note = Note.objects.create(
+            title='title_note_1',
+            text='text_note_1',
+            slug='note_1',
+            author=cls.note_author,
+        )
+
+        cls.new_note_2_data = {
+            'title': 'title_note_2',
+            'text': 'text_note_2',
+            'slug': 'note_2',
         }
 
     def test_note_creation(self):
@@ -40,13 +48,13 @@ class TestLogic(TestCase):
                     self.client.force_login(self.note_author)
 
                     notes_count = Note.objects.count()
-                    self.assertEqual(notes_count, 0)
+                    self.assertEqual(notes_count, 1)
 
-                    response = self.client.post(url, data=self.new_note_1_data)
+                    response = self.client.post(url, data=self.new_note_2_data)
                     self.assertRedirects(response, reverse('notes:success'))
 
                     notes_count = Note.objects.count()
-                    self.assertEqual(notes_count, 1)
+                    self.assertEqual(notes_count, 2)
 
                     created_note = Note.objects.get()
                     self.assertEqual(
@@ -56,39 +64,41 @@ class TestLogic(TestCase):
                             created_note['slug']
                         ),
                         (
-                            self.new_note_1_data['title'],
-                            self.new_note_1_data['text'],
-                            self.new_note_1_data['slug'],
+                            self.new_note_2_data['title'],
+                            self.new_note_2_data['text'],
+                            self.new_note_2_data['slug'],
                         )
                     )
 
                 elif scenario == 'unauthorized_user':
 
                     notes_count = Note.objects.count()
-                    self.assertEqual(notes_count, 0)
+                    self.assertEqual(notes_count, 1)
 
-                    self.client.post(url, data=self.new_note_1_data)
+                    self.client.post(url, data=self.new_note_2_data)
 
-                    response = self.client.post(url, data=self.new_note_1_data)
+                    response = self.client.post(url, data=self.new_note_2_data)
                     redirect_url = f'{self.login_url}?next={url}'
                     self.assertRedirects(response, redirect_url)
 
                     notes_count = Note.objects.count()
-                    self.assertEqual(notes_count, 0)
+                    self.assertEqual(notes_count, 1)
 
     def test_note_creation_with_the_same_slug(self):
         url = reverse('notes:add')
         self.client.force_login(self.note_author)
 
         notes_count = Note.objects.count()
-        self.assertEqual(notes_count, 0)
-
-        self.client.post(url, data=self.new_note_1_data)
-
-        notes_count = Note.objects.count()
         self.assertEqual(notes_count, 1)
 
-        self.client.post(url, data=self.new_note_1_data)
+        self.client.post(
+            url,
+            data={
+                'title': self.note.title,
+                'text': self.note.text,
+                'slug': self.note.slug,
+            }
+        )
         self.assertRaises(ValidationError)
 
         notes_count = Note.objects.count()
@@ -98,16 +108,20 @@ class TestLogic(TestCase):
         url = reverse('notes:add')
         self.client.force_login(self.note_author)
 
-        self.new_note_1_data.pop('slug')
-        response = self.client.post(url, data=self.new_note_1_data)
+        self.new_note_2_data.pop('slug')
+        response = self.client.post(url, data=self.new_note_2_data)
         self.assertRedirects(response, reverse('notes:success'))
 
         notes_count = Note.objects.count()
-        self.assertEqual(notes_count, 1)
+        self.assertEqual(notes_count, 2)
 
-        created_note = Note.objects.get()
-        expected_slug = pytils.translit.slugify(self.new_note_1_data['title'])
+        created_note = Note.objects.get(id=2)
+        expected_slug = pytils.translit.slugify(self.new_note_2_data['title'])
         self.assertEqual(created_note.slug, expected_slug)
 
-
+    def test_other_user_cant_delete(self):
+        self.client.force_login(self.authorised_user)
+        url = reverse('notes:delete', args=(self.note.slug, ))
+        response = self.client.post(url)
+        self.assertEqual(response.status_code, HTTPStatus.NOT_FOUND)
 
