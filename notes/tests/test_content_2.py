4612e072fe46ab26229d4c61308c7ecae67dc0f0
ikpolushin <ikpolushin@yandex.ru>
2026-04-03 10:46:24 +0300

    test note form is in page

--- notes/tests/test_content_2.py
+++ notes/tests/test_content_2.py
@@ -3,6 +3,7 @@ from django.urls import reverse
 from django.test import TestCase
 
 from notes.models import Note
+from notes.forms import NoteForm
 
 User = get_user_model()
 
@@ -39,3 +40,15 @@ class TestRoutes(TestCase):
                     self.assertIn(self.note, object_list)
                 elif scenario == 'authorised_user':
                     self.assertNotIn(self.note, object_list)
+
+    def test_form_in_page(self):
+        test_sets = (
+            ('add_note', 'notes:add', None),
+            ('edit_note', 'notes:edit', (self.note.slug, )),
+        )
+
+        for scenario, url, url_args in test_sets:
+            self.client.force_login(self.note_author)
+            url = reverse(url, args=url_args)
+            response = self.client.get(url)
+            self.assertIsInstance(response.context['form'], NoteForm)
